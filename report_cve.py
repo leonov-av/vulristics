9e8ce63c8d785c50815e13bea9fdd5bce18cf7b9
leonov-av <alexander.v.leonov@gmail.com>
2021-03-07 05:07:05 +0300

    bug - profile path

--- report_cve.py
+++ report_cve.py
@@ -9,11 +9,12 @@ cves_text = '''CVE-2021-3156
 CVE-2021-21972
 CVE-2021-21973
 CVE-2021-21974'''
-data_sources = ['vulners']
+data_sources = ['nvd']
 comments = []
 
 rewrite_flag = True
 
 profile_file_path = "data/profiles/" + file_name
-functions_profile.save_profile(file_name, report_id, report_name, file_name_prefix, cves_text, data_sources, comments)
+functions_profile.save_profile(profile_file_path, report_id, report_name, file_name_prefix, cves_text,
+                               data_sources, comments)
 functions_report_vulnerabilities.make_vulnerability_report_for_profile(profile_file_path, rewrite_flag)
