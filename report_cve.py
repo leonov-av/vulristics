65715dea6fe728e74fa6837c15ce21526ca2dd97
Alex Batishchev <aa.batishev@interfax.ru>
2021-08-19 13:04:53 +0300

    gitignore fix + cves from file

--- report_cve.py
+++ report_cve.py
@@ -4,17 +4,8 @@ import functions_report_vulnerabilities
 report_name = 'CVE NCSC JOINT report'
 # https://www.ncsc.gov.uk/news/joint-advisory-further-ttps-associated-with-svr-cyber-actors
 file_name_prefix = "cve_ncsc_joint"
-cves_text = '''CVE-2018-13379
-CVE-2019-1653
-CVE-2019-2725
-CVE-2019-9670
-CVE-2019-11510
-CVE-2019-19781
-CVE-2019-7609
-CVE-2020-4006
-CVE-2020-5902
-CVE-2020-14882
-CVE-2021-21972'''
+with open('csv_list_to_analyze.txt', 'r') as file:
+    cves_text = file.read()
 
 file_name = "test_cve_profile.json"
 report_id = "test_cve_report"
