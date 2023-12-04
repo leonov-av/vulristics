7117f9cad76b493151070ba8b48bfd01d736593b
Alexander Leonov <alexander.v.leonov@gmail.com>
2023-12-05 00:43:27 +0300

    CPE classificator

--- process_classify_cpe.py
+++ process_classify_cpe.py
@@ -3,42 +3,38 @@ import zipfile
 import xml.etree.ElementTree as ET
 import json
 
-def xml_to_dict(xml_string):
-    root = ET.fromstring(xml_string)
-    json_data = {}
-    json_data[root.tag] = parse_element(root)
-    return json_data
+def get_xml_content(zip_path):
+    # Open the zip file
+    with zipfile.ZipFile(zip_path, "r") as zip_ref:
+        # Get the names of all files in the zip
+        file_names = zip_ref.namelist()
 
-def parse_element(element):
-    if len(element) == 0:
-        return element.text
-    else:
-        data = {}
-        for child in element:
-            child_data = parse_element(child)
-            if child.tag in data:
-                if type(data[child.tag]) is list:
-                    data[child.tag].append(child_data)
-                else:
-                    data[child.tag] = [data[child.tag], child_data]
+        # Read the text from the first file in the zip
+        with zip_ref.open(file_names[0]) as file:
+            xml_content = file.read().decode("utf-8")
+    return xml_content
+
+
+cpe_dict_url = "https://nvd.nist.gov/feeds/xml/cpe/dictionary/official-cpe-dictionary_v2.3.xml.zip"
+cpe_dict_zip_path = "data/classification/cpe_dictionary.zip"
+# urllib.request.urlretrieve(cpe_dict_url, cpe_dict_zip_path)
+# xml_content = get_xml_content(cpe_dict_zip_path)
+# root = ET.fromstring(xml_content)
+
+# Debug
+tree = ET.parse('data/classification/small.xml')
+root = tree.getroot()
+
+cpe_dict = dict()
+for root_child in root:
+    if root_child.tag == "{http://cpe.mitre.org/dictionary/2.0}cpe-item":
+        cpe_id = root_child.attrib['name']
+        cpe_dict[cpe_id] = dict()
+        for cpe_item_child in root_child:
+            if cpe_item_child.tag == "{http://cpe.mitre.org/dictionary/2.0}title":
+                cpe_dict[cpe_id]['title'] = cpe_item_child.text
+            elif cpe_item_child.tag == "{http://scap.nist.gov/schema/cpe-extension/2.3}cpe23-item":
+                cpe_dict[cpe_id]['cpe23-item'] = cpe_item_child.attrib['name']
             else:
-                data[child.tag] = child_data
-        return data
-
-# URL of the zip file
-url = "https://nvd.nist.gov/feeds/xml/cpe/dictionary/official-cpe-dictionary_v2.3.xml.zip"
-
-# Download the zip file
-urllib.request.urlretrieve(url, "data/classification/cpe_dictionary.zip")
-
-# Open the zip file
-with zipfile.ZipFile("data/classification/cpe_dictionary.zip", "r") as zip_ref:
-    # Get the names of all files in the zip
-    file_names = zip_ref.namelist()
-
-    # Read the text from the first file in the zip
-    with zip_ref.open(file_names[0]) as file:
-        xml_content = file.read().decode("utf-8")
-
-dict_data = xml_to_dict(xml_content)
-print(dict_data['{http://cpe.mitre.org/dictionary/2.0}cpe-list']['{http://cpe.mitre.org/dictionary/2.0}cpe-item'])
+                print(cpe_item_child.tag)
+print(cpe_dict)
